600a669f9052cac49f9b3eedde1fb991b820407c
Obata Kohei <ocelot.12.ok@gmail.com>
2026-01-29 12:57:48 +0900

    biquad

--- src/filter/biquad.rs
+++ src/filter/biquad.rs
@@ -1,3 +1,5 @@
+use crate::constants::*;
+
 pub struct Biquad {
     // 係数
     b0: f32,
@@ -43,4 +45,66 @@ impl Biquad {
 
         y
     }
+
+    pub fn set_lpf(&mut self, cutoff: f32, q: f32, fs: f32) {
+        let w0 = 2.0 * PI * cutoff / fs;
+        let cos = w0.cos();
+        let sin = w0.sin();
+        let alpha = sin / (2.0 * q);
+
+        let b0 = (1.0 - cos) * 0.5;
+        let b1 = 1.0 - cos;
+        let b2 = (1.0 - cos) * 0.5;
+        let a0 = 1.0 + alpha;
+        let a1 = -2.0 * cos;
+        let a2 = 1.0 - alpha;
+
+        self.b0 = b0 / a0;
+        self.b1 = b1 / a0;
+        self.b2 = b2 / a0;
+        self.a1 = a1 / a0;
+        self.a2 = a2 / a0;
+    }
+
+    pub fn set_hpf(&mut self, cutoff: f32, q: f32, fs: f32) {
+        let w0 = 2.0 * PI * cutoff / fs;
+        let cos = w0.cos();
+        let sin = w0.sin();
+        let alpha = sin / (2.0 * q);
+
+        let b0 = (1.0 + cos) * 0.5;
+        let b1 = -(1.0 + cos);
+        let b2 = (1.0 + cos) * 0.5;
+        let a0 = 1.0 + alpha;
+        let a1 = -2.0 * cos;
+        let a2 = 1.0 - alpha;
+
+        self.b0 = b0 / a0;
+        self.b1 = b1 / a0;
+        self.b2 = b2 / a0;
+        self.a1 = a1 / a0;
+        self.a2 = a2 / a0;
+    }
+
+    pub fn set_bpf(&mut self, freq: f32, q: f32, fs: f32) {
+        let w0 = 2.0 * PI * freq / fs;
+        let cos = w0.cos();
+        let sin = w0.sin();
+        let alpha = sin / (2.0 * q);
+
+        let b0 = alpha;
+        let b1 = 0.0;
+        let b2 = -alpha;
+        let a0 = 1.0 + alpha;
+        let a1 = -2.0 * cos;
+        let a2 = 1.0 - alpha;
+
+        self.b0 = b0 / a0;
+        self.b1 = b1 / a0;
+        self.b2 = b2 / a0;
+        self.a1 = a1 / a0;
+        self.a2 = a2 / a0;
+    }
+
+
 }
